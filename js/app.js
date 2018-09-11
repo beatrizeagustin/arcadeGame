fa8e454d37c10473ac6857636af70b352dc64fcc
Beatrize <heybeatrize@gmail.com>
2018-09-11 10:18:36 -0700

    initialize enemy class with x y coor

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(positionX, positionY, velocity) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x: positionX;
+    this.y: positionY;
+    this.velocity = velocity;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +17,8 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.velocity * dt
+    
 };
 
 // Draw the enemy on the screen, required method for game
