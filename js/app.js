5962af8941b719508bf93ca2ed4c162b74a51b42
Beatrize <heybeatrize@gmail.com>
2018-09-15 13:43:12 -0700

    fixed scoring method and added restart button plus game sounds

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
+let scores = 0;
 // Enemies our player must avoid
 var Enemy = function(xPos, yPos) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -10,9 +10,7 @@ var Enemy = function(xPos, yPos) {
     this.y = yPos;
     this.move = 101; // moves 101px right
     this.speed = 200; // initial speed
-    this.restartPos = -150; // restarts position
-    
-  
+    this.restartPos = -150; // restarts position 
 };
 
 // Update the enemy's position, required method for game
@@ -21,14 +19,13 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-     this.x += this.speed * dt;
+    this.x += this.speed * dt;
    
     // if enemy reaches 500px at the end of the board
-      if (this.x > 500) {
+    if (this.x > 500) {
         this.x = this.restartPos; 
         // sets random speed after first render
-        this.speed = 150 + Math.floor(Math.random() * this.speed);
-         
+        this.speed = 150 + Math.floor(Math.random() * this.speed);   
     } 
 };
 
@@ -50,28 +47,38 @@ class User {
     }
     
     reset() {
+        // resets player at starting position
         this.x = this.startX;
         this.y = this.startY;
-       
         // removes star animation css property as a reset
         setTimeout(() => { 
             document.getElementById("stars").style.animation = "";
-        }, 400)
-        
+        }, 400) 
     }
     
     score() {
-        if (this.y < 41.5) {
-        let score = 0;
-            score++
-            document.getElementById("score").innerHTML = score;
-             document.getElementById("stars").style.animation = "win 0.5s linear";
-            setTimeout(() => {
-            this.reset()
-        }, 200) 
-        }
+        // plays win sound
+        winSound.play()
+        // adds 1 point and animation  
+        scores++
+        document.getElementById("score").innerHTML = scores;
+        document.getElementById("stars").style.animation = "win 0.5s linear";
+        
+      if (scores === 1) {
+            // appends score reset button
+            const btn = document.createElement('button');
+            const text = document.createTextNode('reset');      
+            btn.appendChild(text);                              
+            document.getElementById('reset_button').appendChild(btn); 
+            btn.onclick = function() {
+                scores = 0
+                document.getElementById("score").innerHTML = scores;
+                document.getElementById('reset_button').innerHTML = '';
+            }
+      }
     }
 }
+
 // draws User on the canvas. You can place this inside User class
 // can put this in user class
 User.prototype.render = function() {
@@ -81,6 +88,9 @@ User.prototype.render = function() {
 // handles users key moves - jumps every 101px x 83px
 // can put this inside user class
 User.prototype.handleInput = function(moves) {
+    if (moves == 'left' || moves == 'up' || moves == 'right' || moves == 'down') {
+        jumpSound.play()
+    }
     // moves left 101px and prevents going off the board
      if (moves == 'left' && this.x > 0) {
          this.x -= 101;
@@ -97,15 +107,14 @@ User.prototype.handleInput = function(moves) {
     if (moves == 'down' && this.y < 395) {
         this.y += 85;
     }
-    // prevent User from going into the water/blue blocks
-    // resets
-  
-        // resets and adds star animation
-    this.score() 
-    
-      
-        // time and score ++ 
-   
+    // prevent User from going into/beyond the blue blocks
+    // resets and adds star score animation
+    if (this.y < 41.5) {
+        this.score() 
+        setTimeout(() => {
+            this.reset();
+        }, 100) 
+    }
 }
 // updates position
 // can put this insude user class
@@ -113,21 +122,41 @@ User.prototype.update = function() {
     //check if user x y position collides with each enemy's 
     for (let enemy of allEnemies) {
         // if User and Enemy's y coordinates match
-        // if '' x plus 80px 
+        // if '' x falls between own x plus 80px 
         if (this.y == enemy.y && this.x < enemy.x + 80 && enemy.x < this.x + 80) {
             // resets
             setTimeout(() => {
                 this.reset()
-                score - 1
-            }, 200) 
+                // plays lose sound
+                loseSound.play()
+                // sets lose animation
+                document.getElementById("stars").style.animation = "lose 0.5s linear";
+            }, 100) 
         }
+    }
+}
+
+// sound constructor from w3cSchools
+class sound {
+    constructor(src) {
+        this.sound = document.createElement("audio");
+        this.sound.src = src;
+        this.sound.setAttribute("preload", "auto");
+        this.sound.setAttribute("controls", "none");
+        this.sound.style.display = "none";
+        document.body.appendChild(this.sound);
+        this.play = function(){
+            this.sound.play();
+        }
+        this.stop = function(){
+            this.sound.pause();
+        } 
     }
 }
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
 const player = new User();
 const enemy1 = new Enemy(101, 55);
 const enemy2 = new Enemy(99, 55);
@@ -137,7 +166,10 @@ const enemy5 = new Enemy(50, 225);
 const allEnemies = [];
 // push new enemies into allEnemies
 allEnemies.push(enemy1, enemy2, enemy3, enemy4, enemy5);
-
+// sounds from freesounds.com
+let jumpSound = new sound('sounds/167045__drminky__slime-jump.wav')
+let winSound = new sound('sounds/350875__cabled-mess__coin-c-07.wav');
+let loseSound = new sound('sounds/233343__otisjames__squitch.wav');
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
